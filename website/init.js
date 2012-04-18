2effd2b9178ae55479d5fb8dc68ce3aa60091ddb
Daniel Schlaug <daniel.schlaug@gmail.com>
2012-04-19 01:02:53 +0200

    added DataTables and improved layout

--- website/init.js
+++ website/init.js
@@ -1,15 +1,8 @@
 
 $(function() {
 	$( "#maintabs" ).tabs({
-		ajaxOptions: {
-			error: function( xhr, status, index, anchor ) {
-				$( anchor.hash ).html(
-					"Ojoj, ajaxfel. Illa illa dschlaug@kth.se är förmodligen skyldig." );
-			}
-		},
-//		cookie: {
-			// store cookie for a 365 days, without, it would be a session cookie
-//			expires: 365
-//		}
+		cookie: {
+			expires: 365
+		}
 	});
 });
