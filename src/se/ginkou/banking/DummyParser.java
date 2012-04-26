d46b3751a04276314ec35a88bce37ed79dee98e1
Frans Tegelmark <freus92@gmail.com>
2012-04-27 00:00:40 +0200

    work on dummyparser

--- src/se/ginkou/banking/DummyParser.java
+++ src/se/ginkou/banking/DummyParser.java
@@ -1,11 +1,19 @@
 package se.ginkou.banking;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 import org.webharvest.definition.ScraperConfiguration;
 import org.webharvest.runtime.Scraper;
+import org.webharvest.runtime.ScraperContext;
 import org.webharvest.runtime.variables.Variable;
 public class DummyParser {
 	public static void main(String[] args) {
+	    /** Prefix for any variable scraped which defines a URL. It will be followed by a counter. */  
+	    final String SCRAPED_URL_VARIABLE_PREFIX = "url.";  
+	    /** A variable name which holds the initial URL to scrape. */  
+	    final String START_URL_VARIABLE = "url";  
+		
+		
 		// register external plugins if there are any
 		//DefinitionResolver.registerPlugin("com.my.MyPlugin1");
 
@@ -20,11 +28,24 @@ public class DummyParser {
 
 			scraper.execute();
 
-			// takes variable created during execution
-			scraper.getContext()
+			// takes variable created during execution			
+			ScraperContext context = scraper.getContext();  
+			System.out.println(context.size());
+			System.out.println(context);
+			
+			ArrayList<String> al = new ArrayList<String>();
+            int i=1;  
+            Variable scrapedUrl;  
+            while ((scrapedUrl = (Variable) context.get(SCRAPED_URL_VARIABLE_PREFIX+i))  != null)  
+            {  
+                // parse this URL  
+                al.add(scrapedUrl.toString());  
+                // move on  
+                i++;  
+            }  
 
 			// do something with articles...
-			//System.out.println(articles.isEmpty());
+			System.out.println(al);
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
