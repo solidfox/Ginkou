e4c1778501195ec9b8079691c7fb5d25ee3d0395
Frans Tegelmark <freus92@gmail.com>
2012-04-27 06:12:40 +0200

    seb parser done

--- src/se/ginkou/banking/DummyParser.java
+++ src/se/ginkou/banking/DummyParser.java
@@ -1,25 +1,28 @@
 package se.ginkou.banking;
 import java.io.FileNotFoundException;
+import java.text.DateFormat;
 import java.util.ArrayList;
 
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.DateTimePrinter;
 import org.webharvest.definition.ScraperConfiguration;
 import org.webharvest.runtime.Scraper;
 import org.webharvest.runtime.ScraperContext;
 import org.webharvest.runtime.variables.Variable;
+
+import se.ginkou.Account;
+import se.ginkou.Transaction;
 public class DummyParser {
 	public static void main(String[] args) {
-	    /** Prefix for any variable scraped which defines a URL. It will be followed by a counter. */  
-	    final String SCRAPED_URL_VARIABLE_PREFIX = "url.";  
-	    /** A variable name which holds the initial URL to scrape. */  
-	    final String START_URL_VARIABLE = "url";  
-		
-		
+	
 		// register external plugins if there are any
 		//DefinitionResolver.registerPlugin("com.my.MyPlugin1");
 
 		ScraperConfiguration config;
 		try {
-			config = new ScraperConfiguration("test.xml");
+			config = new ScraperConfiguration("sebscrapper.xml");
 			Scraper scraper = new Scraper(config, ".");
 			scraper.addVariableToContext("username", "web-harvest");
 			scraper.addVariableToContext("password", "web-harvest");
@@ -27,26 +30,35 @@ public class DummyParser {
 			//scraper.setDebug(true);
 
 			scraper.execute();
-
+			
 			// takes variable created during execution			
-			ScraperContext context = scraper.getContext();  
+			ScraperContext context = scraper.getContext();
+			System.out.println(scraper);
 			System.out.println(context.size());
-			System.out.println(context);
+			//System.out.println(context);
 			
-			ArrayList<String> al = new ArrayList<String>();
-            int i=1;  
-            Variable scrapedUrl;  
-            while ((scrapedUrl = (Variable) context.get(SCRAPED_URL_VARIABLE_PREFIX+i))  != null)  
-            {  
-                // parse this URL  
-                al.add(scrapedUrl.toString());  
-                // move on  
-                i++;  
-            }  
+			ArrayList<Transaction> al = new ArrayList<Transaction>();
+			int i=1;  
+			Variable account, date, notice, amount;
+			if((account = (Variable) context.get("account"))  != null){
 
+				while ((date = (Variable) context.get("date."+i))  != null && 
+						(notice = (Variable) context.get("notice."+i))  != null &&
+						(amount = (Variable) context.get("amount."+i))  != null){
+					al.add(new Transaction(new Account(account.toString()), 
+							DateTime.parse(date.toString().trim(), DateTimeFormat.forPattern("yyMMdd")), 
+							notice.toString(), 
+							Double.parseDouble(amount.toString().replace(".", "").replace(",", "."))));
+					i++;  
+				}
+			}
+            i = 0;
+            for(Transaction t: al){
+            	System.out.println(i +": "+t.toString());
+            	++i;
+            }
+            
 			// do something with articles...
-			System.out.println(al);
-			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
