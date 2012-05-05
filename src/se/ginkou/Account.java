9bfbcb6b0e5faaffe34043ea10f9df215fc7ecb1
Daniel Schlaug <daniel.schlaug@gmail.com>
2012-05-05 15:42:11 +0200

    Brushed up the accounts class, changed the account number to an int and made it the id of the class on the premise that the same user won't have two equal account numbers to different accounts.

--- src/se/ginkou/Account.java
+++ src/se/ginkou/Account.java
@@ -1,41 +1,96 @@
 package se.ginkou;
 
+import java.util.HashMap;
+
+import se.ginkou.database.SQLiteDB;
+
 public class Account {
 	
-	int id;
-	String accountNumber;
-	String accountName;
+	private final int number; // Unique account number
+	private String name; // Optional name of the account, can be null.
+	
+	// Cache of accounts loaded from database 
+	private static HashMap<Integer, Account> cache = new HashMap<Integer, Account>();
 
-	public Account(int accountId) {
-		id = accountId;
+	private Account(int accountNumber, String accountName) {
+		this.number = accountNumber;
+		this.name = accountName;
+	}
+	
+	/**
+	 * Returns an Account object with the given number.
+	 * If there already exists transactions with the given account number
+	 * the existing account is returned. There is no guarantee that the given
+	 * account name will be used.
+	 * @param accountNumber the unique number of the account.
+	 * @param accountName used to name a new account if none already exists, 
+	 * if null the account is given not given a name.
+	 * @return an Account object with the given account number.
+	 */
+	public static Account newAccount(int accountNumber, String accountName) {
+		Account acc = getAccount(accountNumber);
+		if (acc != null) {return acc;}
+		
+		acc = new Account(accountNumber, accountName);
+		Account.cache.put(acc.getNumber(), acc);
+		return acc;
+	}
+	
+	/**
+	 * Returns the sought account object if it exists in the database, 
+	 * otherwise returns null.
+	 * @param accountNumber the account number of the sought account.
+	 * @return the sought account object if it exists in the database, 
+	 * otherwise null.
+	 */
+	public static Account getAccount(int accountNumber) {
+		if (Account.cache.containsKey(accountNumber)) {
+			return Account.cache.get(accountNumber);
+		}
+		return Account.loadFromDatabase(accountNumber);
 	}
 
-	public Account(String accountNumber) {
-		this.accountNumber = accountNumber;
-		// TODO generate unique accountID
+	private static Account loadFromDatabase(int accountNumber) {
+		Account[] fromDB = SQLiteDB.getDB().getAccounts();
+		for (Account acc : fromDB) {
+			Account.cache.put(acc.getNumber(), acc);
+		}
+		return Account.cache.get(accountNumber);
 	}
 
-	public int getID() {
-		return id;
+	/**
+	 * Returns the account number.
+	 * @return the account number.
+	 */
+	public int getNumber() {
+		return number;
+	}
+	
+	/**
+	 * Returns the name of the account or the empty string if it has no name.
+	 * @returnthe name of the account or the empty string if it has no name.
+	 */
+	public String getName() {
+		return (name == null ? "" : name);
 	}
 
-	public String getAccountNumber() {
-		return accountNumber;
-	}
-
+	/**
+	 * Returns a string containing information about the account.
+	 */
 	@Override
 	public String toString() {
-		return accountNumber + (accountName != null ? " " + accountName : "");
+		return number + (name != null ? " " + name : "");
 	}
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		return result;
+		return number;
 	}
 
+	/**
+	 * Returns true if obj is an account with the same account number
+	 * as this one. Otherwise false.
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -45,9 +100,8 @@ public class Account {
 		if (getClass() != obj.getClass())
 			return false;
 		Account other = (Account) obj;
-		if (id != other.id)
+		if (number != other.number)
 			return false;
 		return true;
 	}
-
 }
