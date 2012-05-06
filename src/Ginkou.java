7c9c2c252364754bb36894071b646421d4853a6c
Daniel Schlaug <daniel.schlaug@gmail.com>
2012-05-06 17:27:39 +0200

    Made Ginkou autostart the webpage and server

--- src/Ginkou.java
+++ src/Ginkou.java
@@ -1,10 +1,11 @@
-import java.io.*;
-import java.net.*;
+import java.awt.Desktop;
+import java.net.URI;
 
 import se.ginkou.interfaceio.InterfaceServer;
 
 class Ginkou {
 	public static void main(String args[]) throws Exception {
+		Desktop.getDesktop().browse(new URI("http://ginkou.se"));
 		InterfaceServer.startServer();
 	}
 }
